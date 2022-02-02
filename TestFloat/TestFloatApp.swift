947d3365a2e91dddd57f220a0139ea435aee1402
Code Palace <codepalace@fastmail.se>
2022-02-02 11:25:37 +0100

    Initial Commit

---
+++ TestFloat/TestFloatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFloatApp.swift
+//  TestFloat
+//
+//  Created by Federico on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestFloatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
