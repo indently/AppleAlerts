5c0cbbd114adac8e64eebf268bbea3fbf187e72e
Code Palace <codepalace@fastmail.se>
2022-02-02 12:08:02 +0100

    Hello World!

--- TestFloat/ContentView.swift
+++ TestFloat/ContentView.swift
@@ -6,11 +6,43 @@
 //
 
 import SwiftUI
+import SPAlert
 
 struct ContentView: View {
+    @State var showAlert = false
+    @State var showAlert2 = false
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            Button("Show Alert") {
+                showAlert.toggle()
+            }
+            .SPAlert(
+                isPresent: $showAlert,
+                message: "Learning is easy!",
+                duration: 1.0)
+            
+            Button("Show Alert 2") {
+                showAlert2.toggle()
+            }
+            .SPAlert(
+                isPresent: $showAlert2,
+                title: "Code Palace",
+                message: "This tutorial has been completed",
+                duration: 2.0,
+                dismissOnTap: false,
+                preset: .done,
+                haptic: .success,
+                layout: .init(),
+                completion: {
+                    print("This is a completion log.")
+                    showAlert.toggle()
+                })
+            .tint(.orange)
+        }
+        
+        // For custom image add (preset) : .custom(UIImage(systemName: "leaf")!)
     }
 }
 
